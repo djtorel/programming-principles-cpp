3df5784feb76d3842812330e6faaf6167fd1daed
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-22 12:45:45 -0700

    Chapter 1, exercises 1 and 2 done

---
+++ chap01/exercises/ex01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, programming!" << endl;
+  cout << "Here we go!" << endl;
+  return 0;
+}
---
+++ chap01/exercises/ex02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "To find up stairs bathroom:" << endl;
+  cout << "1) Perform standup routine" << endl;
+  cout << "2) Perform turn left routine" << endl;
+  cout << "3) Perform walking routine for 4 feet" << endl;
+  cout << "4) Perform turn right routine" << endl;
+  cout << "5) Perform walking routine for 5 feet" << endl;
+  cout << "6) Perform walking up stairs routine, until at top of stairs" << endl;
+  cout << "7) Perform walking routine for 3 feet" << endl;
+  cout << "8) Perform turn left routine" << endl;
+}
