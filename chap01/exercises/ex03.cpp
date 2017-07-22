35e0093c9aa04dbebae25c9f65993f3296c0cabd
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-22 14:56:11 -0700

    Finished exercise 3

---
+++ chap01/exercises/ex03.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main() {
+  cout << "Instructions to get to McDonalds" << endl;
+  cout << "Walk 10 feet and turn left" << endl;
+  cout << "Walk down stairs turn left" << endl;
+  cout << "Walk down more stairs, turn left" << endl;
+  cout << "Walk down more stairs, turn left" << endl;
+  cout << "Walk down yet more stairs then turn right" << endl;
+  cout << "Walk forward 10 feet and turn left" << endl;
+  cout << "Walk forward 50 feet get in car" << endl;
+  cout << "Turn car on and put it in reverse" << endl;
+  cout << "If way is clear back up, turning wheel right" << endl;
+  cout << "Turn wheel left and put it into drive, apply gas gently" << endl;
+  cout << "Look for cars coming from left, if clear turn right and apply gas" << endl;
+  cout << "After 100 feet, look for cars coming right, if clear turn right and apply gas" << endl;
+  cout << "Go straight until at light, when green turn left" << endl;
+  cout << "Go straight until at next light, take left at light" << endl;
+  cout << "Go straight 100 feet and take right into McDonalds parking lot" << endl;
+}
