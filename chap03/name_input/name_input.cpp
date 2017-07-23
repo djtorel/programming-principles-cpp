727ebe9f6cf1c0545190d5004e73a5c8f0679b0b
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-22 19:09:06 -0700

    More name_input stuff

--- chap03/name_input/name_input.cpp
+++ chap03/name_input/name_input.cpp
@@ -1,8 +1,14 @@
 #include "std_lib_facilities.h"
 
 int main() {
-  cout << "Please enter your first name (folowed by 'enter'):\n";
-  string first_name;    // first_name is a variable of type string
-  cin >> first_name;    // read characters into first_name
-  cout << "Hello, " << first_name << "!\n";
+  cout << "Please enter your first and second name (folowed by 'enter'):\n";
+  string first_name = "???";    // first_name is a variable of type string
+                                // ("???" means "don't know the name")
+  string second_name = "???";
+  cin >> first_name >> second_name;    // read characters into first_name
+  cout << "Please enter your age: ";
+  int age = -1;   // integer variable (-1 means "don't know age")
+  cin >> age;
+  int age_months = age * 12;
+  cout << "Hello, " << first_name << " " << second_name << " (age " << age << " years, or " << age_months << " months)!\n";
 }
