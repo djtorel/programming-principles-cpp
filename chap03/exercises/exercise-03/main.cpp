2cf14757c2d06f476c63f546bfb75ec8456d0526
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-29 21:07:02 -0700

    Finished chapter 3, exercise 3

---
+++ chap03/exercises/exercise-03/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Write a program that doesn't do anything, but declares a number of variables
+ * with legal and illegal names so you can see how the compiler reacts
+ */
+
+int main() {
+  /* int double = 0;
+  int 1piece = 1;
+  char int = '\n';
+  double money$!money = 3.50; */
+
+  int foo = 39;
+  double money$money = 3.50;
+}
